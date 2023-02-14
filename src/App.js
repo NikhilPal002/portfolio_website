2039d56012d5570bc27b82ceb34d9eb2db4b5da0
Nikhil <nikhilpal2017@gmail.com>
2023-02-14 21:59:13 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import Preloader from "../src/components/Pre"
+// import logo from './logo.svg';
 import './App.css';
+import Home from "./components/Home/Home";
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Navigate
+} from 'react-router-dom';
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from 'react-bootstrap';
 
 function App() {
+  const [load, upadateLoad] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Preloader load={load}/>
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar/>
+        <ScrollToTop/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </div>
+
+    </Router>
+    
+      
   );
 }
 
