b3f857a66f05059ce9a3036c0a0176e304fa918c
Nikhil <nikhilpal2017@gmail.com>
2023-02-15 21:25:35 +0530

    fixed resume link

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,8 +9,8 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/NikhilPal002/portfolio_website/raw/master/src/Assets/Nikhil_Pal-Resume.pdf";
-  // "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+"https://raw.githubusercontent.com/NikhilPal002/portfolio_website/master/src/Assets/Nikhil_Pal-Resume.pdf";
+// "https://github.com/NikhilPal002/portfolio_website/raw/master/src/Assets/Nikhil_Pal-Resume.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
